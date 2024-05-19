260c5294e7c2b3079ad9a3aca85dc49940acd55d
cinmon <150573808+cinmon@users.noreply.github.com>
2024-05-19 15:30:44 +0900

    I don't remember well

--- ComposeQuadrant/app/src/main/java/com/gianella/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/gianella/composequadrant/MainActivity.kt
@@ -3,14 +3,27 @@ package com.gianella.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gianella.composequadrant.ui.theme.ComposeQuadrantTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   ComposeQuadrant()
                 }
             }
         }
@@ -30,17 +43,128 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeQuadrant(modifier : Modifier = Modifier) {
+
+    Column(Modifier.fillMaxWidth()){
+        Row(Modifier.weight(1f)) {
+            TextComposable(
+                title = stringResource(R.string.text_composable_title),
+                description = stringResource(R.string.text_composable_description),
+                color = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ImageComposable(
+                title = stringResource(R.string.image_composable_title),
+                description = stringResource(R.string.image_composable_description),
+                color = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row(Modifier.weight(1f)) {
+            RowComposable(
+                title = stringResource(R.string.row_composable_title),
+                description = stringResource(R.string.row_composable_description),
+                color = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ColumnComposable(
+                title = stringResource(R.string.column_composable_title),
+                description = stringResource(R.string.column_composable_description),
+                color = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+
+
+
+}
+
+@Composable
+fun TextComposable(title : String, description : String, color : Color, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color)
+    ){
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            modifier = Modifier
+        )
+    }
+}
+
+@Composable
+fun ImageComposable(title : String, description : String, color : Color, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color)
+    ){
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            modifier = Modifier
+        )
+    }
+}
+
+@Composable
+fun RowComposable(title : String, description : String, color : Color, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color)
+    ){
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            modifier = Modifier
+        )
+    }
+}
+
+@Composable
+fun ColumnComposable(title : String, description : String, color : Color, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color)
+    ){
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            modifier = Modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrant()
     }
 }
